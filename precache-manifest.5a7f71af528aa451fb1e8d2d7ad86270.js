d6db7ec098c2b43215fb456d4e830912aa4135fd
ZachChang <podeng0321@gmail.com>
2019-05-05 18:33:23 +0800

    Updates

---
+++ precache-manifest.5a7f71af528aa451fb1e8d2d7ad86270.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c849c7adf3ea15034d23",
+    "url": "/information-architecture-tool/static/js/runtime~main.2db9217e.js"
+  },
+  {
+    "revision": "f16e6596d3c9e6c86c1b",
+    "url": "/information-architecture-tool/static/js/main.2f6cef0f.chunk.js"
+  },
+  {
+    "revision": "346de1f7a4662e458e7d",
+    "url": "/information-architecture-tool/static/js/2.e8e54dbd.chunk.js"
+  },
+  {
+    "revision": "f16e6596d3c9e6c86c1b",
+    "url": "/information-architecture-tool/static/css/main.3b0a5468.chunk.css"
+  },
+  {
+    "revision": "d96653655899389ef07118fed111cb89",
+    "url": "/information-architecture-tool/index.html"
+  }
+];
